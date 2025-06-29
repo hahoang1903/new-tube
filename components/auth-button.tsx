066610eec033425dc82d2f0e4f460e2939468663
Ha Hoang <37249195+hahoang1903@users.noreply.github.com>
2025-06-30 00:03:40 +0700

    add basic auth functions (#2)
    * add basic auth functions
    
    * add dummy env github actions
    
    * update env for build github actions using secrets

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import { UserCircleIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <Button
+            variant="outline"
+            className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
 
